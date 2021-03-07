da51fce18f65376e5be63b4087578624ab682362
Taras Liuzniak <lyuznyaktaras@gmail.com>
2021-03-07 12:17:33 +0200

    int save
    added memoization

--- src/redux/phonebook/phonebook-selectors.js
+++ src/redux/phonebook/phonebook-selectors.js
@@ -1,15 +1,26 @@
+import { createSelector } from '@reduxjs/toolkit'
 const getLoading = (state) => state.contacts.loading
 const getFilter = (state) => state.contacts.filter
 const getContacts = (state) => state.contacts.items
 
-const getfilteredElements = (state) => {
-    const filter = getFilter(state)
-    const contacts = getContacts(state)
-    const normalizedFilter = filter.toLowerCase()
-    return contacts.filter((contact) =>
-        contact.name.toLowerCase().includes(normalizedFilter)
-    )
-}
+// const getfilteredElements = (state) => {
+//     const filter = getFilter(state)
+//     const contacts = getContacts(state)
+//     const normalizedFilter = filter.toLowerCase()
+//     return contacts.filter((contact) =>
+//         contact.name.toLowerCase().includes(normalizedFilter)
+//     )
+// }
+
+const getfilteredElements = createSelector(
+    [getFilter, getContacts],
+    (filter, contacts) => {
+        const normalizedFilter = filter.toLowerCase()
+        return contacts.filter((contact) =>
+            contact.name.toLowerCase().includes(normalizedFilter)
+        )
+    }
+)
 
 export default {
     getLoading,
